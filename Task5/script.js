c7442c32a9638780dedd5398a880cb40da02323f
simson40024 <simson40024@gmail.com>
2023-01-07 16:28:15 +0200

    completed task3 & task5

---
+++ Task5/script.js
@@ -0,0 +1,13 @@
+let countAttempts = 2;
+let succseed_attempts = 1;
+let FailedAttempts = 0;
+alert (`countAttempts: ${countAttempts}\n`+
+    `succseed_attempts: ${succseed_attempts}\n`+
+    `IFailedAttempts: ${FailedAttempts}`);
+console.log(`camelCase-> countAttempts: ${countAttempts}`);
+console.log(`snake_case-> succseed_attempts: ${succseed_attempts}`);
+console.log(`PascalCase-> IFailedAttempts: ${FailedAttempts}`);
+/*
+Console.log - виводить результат в консоль та продовжує виконання скрипта. <br>
+Alert - виводить результат в модальному вікні та "ставить скрипт на паузу" поки не наниснута кнопка ОК.
+*/
